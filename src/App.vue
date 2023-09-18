338036c444c1c22f747e2a8fa5e2bb3a1a7ab3b4
hyuuto <xkakashi00@gmail.com>
2023-09-18 16:20:07 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,17 @@ const authStore = useAuthStore();
 </script>
 
 <template>
-    <div class="app-container bg-light">
-        <nav v-show="authStore.user" class="navbar navbar-expand navbar-dark bg-dark">
-            <div class="navbar-nav">
-                <RouterLink to="/" class="nav-item nav-link">Home</RouterLink>
-                <a @click="authStore.logout()" class="nav-item nav-link">Logout</a>
-            </div>
-        </nav>
-        <div class="container pt-4 pb-4">
-            <RouterView />
-        </div>
-    </div>
+	<div class="app-container bg-light">
+		<nav v-show="authStore.user" class="navbar navbar-expand navbar-dark bg-dark">
+			<div class="navbar-nav">
+				<RouterLink to="/" class="nav-item nav-link">Home</RouterLink>
+				<a @click="authStore.logout()" class="nav-item nav-link">Logout</a>
+			</div>
+		</nav>
+		<div class="container pt-4 pb-4">
+			<RouterView />
+		</div>
+	</div>
 </template>
 
 <style>
